8b19f62db58e12795a5c5fdc9f21ac32943fb01e
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-16 07:08:37 -0500

    Create  jmol.java file
    Jmol object encapsulation file to main

---
+++ Jmol.java
@@ -0,0 +1,33 @@
+public class jmol extends main {
+  
+  public static final Applet = function;
+    public static final int id = 0;
+    public static final int Info;
+    public static final int checkOnly;
+    
+    private String jmolA;
+    private String jmolB;
+    private int Info;
+    private int Applet
+    
+   public jmol(String OtherjmolA, String OtherjmolB, int OtherInfo, int OtherApplet){
+     
+     jmolA = OtherjmolA;
+     jmolB = OtherjmolB;
+     Info = OtherInfo;
+     setApplet(OtherApplet);
+      
+      public jmol(String OtherjmolA){
+        this(OtherjmolA, "undefined description", Applet, UNDEFINED);
+      }
+      
+      public jmol(String OtherjmolB){
+        this(OtherjmolB, "undefined description", Applet, UNDEFINED);
+      }
+      
+      public void setjmolA (String j){jmolA = j;}
+      public getjmolA(){return jmolA}
+      
+      
+      
+     
