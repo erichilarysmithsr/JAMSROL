59bfe4603fa9697b5ca6695c255da0008c6d5311
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-22 15:13:22 -0500

    Create VaccinePipeline.java

---
+++ VaccinePipeline.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.Lang.Math;
+
+public class VaccinePipeline {
+
+    private List<Vaccine> vaccines = new ArrayList<>();
+
+    public VaccinePipeline() {
+    }
+
+    public void createVaccine(String vaccineName, String vaccineType, String vaccineStatus) {
+        vaccines.add(new Vaccine(vaccineName, vaccineType, vaccineStatus));
+    }
+
+    public void storeVaccine(int vaccineId, Vaccine vaccineData) {
+        vaccines.set(vaccineId, vaccineData);
+    }
+
+    public Vaccine getVaccine(int vaccineId) {
+        return vaccines.get(vaccineId);
+    }
+
+    public List<Vaccine> getAllVaccines() {
+        return vaccines;
+    }
+
+}
+
+class Vaccine {
+
+    private String name;
+    private String type;
+    private String status;
+
+    public Vaccine(String name, String type, String status) {
+        this.name = name;
+        this.type = type;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
